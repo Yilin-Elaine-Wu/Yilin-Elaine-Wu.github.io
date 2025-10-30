b80f0a68773c3f0aa6d37bb71d40b6b35e59289a
Yilin Elaine Wu <yilinelainewu@gmail.com>
2025-10-30 19:55:02 +0800

    Update global data with new profile and contact info

--- data/global.js
+++ data/global.js
@@ -7,12 +7,12 @@ const navbarLinks = {
 };
 
 const globalData = {
-  image: "../files/profile.jpg",
-  resume: "../files/resume.pdf",
+  image: "../files/profile_figure.jpg",
+  resume: "../files/CV_Yilin_Wu_25fall.pdf",
   enAddress: "Your address ",
   faAddress: " آدرس شما ",
-  phone: "09120000000",
-  email: "example@email.com",
+  phone: "+1 (510)-229-2508",
+  email: "yilinelainewu@gmail.com",
   twitterTitle: "",
   twitter: "",
   whatsappTitle: "WhatsApp number",
@@ -21,8 +21,8 @@ const globalData = {
   googleScholar: "@googleScholar",
   linkedinTitle: "Linkedin Username",
   linkedin: "https://www.linkedin.com/in/your_username/",
-  githubTitle: "Github Username",
-  github: "https://github.com/your_username",
-  orcidTitle: "Orcid Username",
-  orcid: "https://orcid.com/your_username",
+  githubTitle: "Yilin-Elaine-Wu",
+  github: "https://github.com/Yilin-Elaine-Wu",
+  orcidTitle: "Yilin Elaine Wu",
+  orcid: "https://orcid.org/my-orcid?orcid=0009-0004-0686-398X",
 };
