6314dde77cfad053bb66b37cc105915d5d1258c2
Yilin Elaine Wu <yilinelainewu@gmail.com>
2025-10-30 20:57:05 +0800

    Refactor name format and enhance home page details
    Updated the name format and improved the home page content. Removed unused job entries and added a script for embedding a PDF CV.

--- lang/en.js
+++ lang/en.js
@@ -1,6 +1,6 @@
 // ---------- Navbar titles
 const enNavbarData = {
-  title: " Yilin (Elaine) Wu ",
+  title: " Yilin Elaine Wu ",
   Home: "Home",
   publications: "Publications",
   Research: "Research",
@@ -10,18 +10,22 @@ const enNavbarData = {
 
 // ---------- Home page data
 const enHomePageData = {
-  name: " Yilin (Elaine) Wu ",
+  name: " Yilin Elaine Wu ",
   jobTitle: " Undergraduate Researcher in Cognitive Science ",
   home_title: "About me",
   home_content: `
   <div>
     <p> Hi! I'm Yilin Wu. I'm an undergraduate majoring in psychology in Nanjing University.
-    I'm interested in human cognition and computational modeling of human minds. I work to understand how human attend to and extract useful information from a complex environment, and how they form abstract mental representations in an effective way.
-    I try to answer these questions in a way of studying the human innate cognitive toolkits including game-playing, sketches and language, primarily by conducting behavioral experiments and building computational models including normative models like Bayesian model and expectedly also neural networks. 
-    I also do some research with children in their early stage of development to investigate how the intricate human minds are built up step by step.
+    I'm interested in human cognition and computational modeling of human minds. I am currently a research intern working in China Institute of Brain Research, supervised by Dr. Ni Ji. And I am seeking for a RA position in cognitive science after my graduation in 2026, hoping to apply for PhD candidates in the 26-27 cycle. In my free time, I enjoy singing, cooking and ASMR videos.
+    
     </p>
     <h2 class='title'> Main works </h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh  </p>
+    <p>
+    I work to understand how human attend to and extract useful information from a complex environment, and how they form abstract mental representations in an effective way.
+    I try to answer these questions in a way of studying the human innate cognitive toolkits including game-playing, sketches and language, primarily by conducting behavioral experiments and building computational models including normative models like Bayesian model and expectedly also neural networks. 
+    I also do some research with children in their early stage of development to investigate how the intricate human minds are built up step by step. I'm looking forward to collaborate with researchers experienced in techniques like eye-tracking, real-life video analysis and neuroimaging as well.
+    Please check the research page for more details in my projects.
+    </p>
     <ul>
       <li> Attention and Learning</li>
       <li> Mental Representations </li>
@@ -34,70 +38,70 @@ const enHomePageData = {
 // ---------- Publications page data
 const enPublicationsPageData = {
   type_one_title: "Conference Contribution",
-
+  subtitle:"Poster Presentation",
   type_one_items: [
     {
-      title: `Dancing to the beat of climate change: Studying visual frames on TikTok through the left-right divide among U.S. politicians (with ).`,
+      title: `Task-space dimensions guide human exploration in complex environments.`,
       abstract: `Politicians across the political spectrum have taken to TikTok in an attempt to reach young voters by producing original videos. This novel trend has turned TikTok into the new polarizing ring in the political communication battle, deepening the partisan breach around issues such as climate change. However`,
-      date: "",
+      date: "2025, August",
       link: "http://link.com",
       github: "",
-      download: "",
-      writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
+      download: "Yilin-Elaine-Wu/files/CCN2025_poster.pdf",
+      writers: "An, J., Hu, J., <b>Wu, Y.E.</b>, Ning, S., Zhu, F., Pan, Y., & Ji, N*",
     },
-    {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `Politicians across the political spectrum have taken to TikTok in an attempt to reach young voters by producing original videos. This novel trend has turned TikTok into the new polarizing ring in the political communication battle, deepening the partisan breach around issues such as climate change. However`,
-      date: "1996",
-      link: "",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla", ""],
-    },
+    // {
+    //   title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+    //   abstract: `Politicians across the political spectrum have taken to TikTok in an attempt to reach young voters by producing original videos. This novel trend has turned TikTok into the new polarizing ring in the political communication battle, deepening the partisan breach around issues such as climate change. However`,
+    //   date: "1996",
+    //   link: "",
+    //   github: "http://github.com",
+    //   writers: ["Claudio Cioffi-Revilla", ""],
+    // },
   ],
 
-  type_two_title: "Under Review",
-  type_two_items: [
-    {
-      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
-      abstract: ``,
-      date: "1996",
-      link: "",
-      github: "",
-      writers: [],
-    },
-    {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
-    },
-  ],
+  // type_two_title: "Under Review",
+  // type_two_items: [
+  //   {
+  //     title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+  //     abstract: ``,
+  //     date: "1996",
+  //     link: "",
+  //     github: "",
+  //     writers: [],
+  //   },
+  //   {
+  //     title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+  //     abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
+  //     date: "1996",
+  //     link: "http://link.com",
+  //     github: "http://github.com",
+  //     writers: ["Claudio Cioffi-Revilla", "masoud Claudio "],
+  //   },
+  // ],
 
-  type_three_title: "Book Chapters",
-  type_three_items: [
-    {
-      title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
-      abstract: ``,
-      date: "",
-      link: "",
-      github: "",
-      writers: [],
-    },
-    {
-      title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
-      abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
-      date: "1996",
-      link: "http://link.com",
-      github: "http://github.com",
-      writers: ["Claudio Cioffi-Revilla"],
-    },
-  ],
+  // type_three_title: "Book Chapters",
+  // type_three_items: [
+  //   {
+  //     title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
+  //     abstract: ``,
+  //     date: "",
+  //     link: "",
+  //     github: "",
+  //     writers: [],
+  //   },
+  //   {
+  //     title: `“Now you are speaking my language” Language-specific transparency and legislative negotiations in the Council of Ministers of the European Union. (with ).`,
+  //     abstract: `we adopt an elite-focused approach to explore the ways in which left and right-leaning American political elites differ in their visual and emotional framing of climate change. Overall, this study provides new insight into how politicians across party lines communicate about critical and polarizing political issues, such as climate change, by adapting to novel online communication channels.`,
+  //     date: "1996",
+  //     link: "http://link.com",
+  //     github: "http://github.com",
+  //     writers: ["Claudio Cioffi-Revilla"],
+  //   },
+  // ],
 
-  type_four_title: "",
-  type_four_items: [
-    {
+  // type_four_title: "",
+  // type_four_items: [
+  //   {
       title: `Elena Reinaga. 2016. If I were born again I would still be a sex worker. P. Purdy and N. Umansky. OpenDemocracy.`,
       abstract: ``,
       date: "",
@@ -121,12 +125,8 @@ const enResearchPageData = {
   title: "Research",
   content: `
     <div class='research_content'>
-        <p> My research goal is to combine the three major sub-fields of the Computational Social Science, including Social Media Data Analysis, Network Analysis, and Agent-based Modeling, to study various social science problems. Below you can find a summary of my past and current projects.</p>
-        <br />
-        <h4 style="font-size: 1.1rem "> Identifying and Characterizing US Domestic Ideological Extremists </h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
-        <img src='../files/images/research.png' />
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra  accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat <br /> imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus. Mattis pellentesque id nibh tortor id aliquet <br /> lectus proin. Sapien faucibus et molestie ac feugiat sed lectus vestibulum. Ullamcorper velit sed ullamcorper morbi tincidunt ornare massa eget. Dictum varius duis at consectetur lorem. Nisi vitae suscipit tellus mauris a diam maecenas sed enim. Velit ut tortor pretium viverra suspendisse potenti nullam. Et molestie ac feugiat sed lectus. Non nisi est sit amet facilisis magna. Dignissim diam quis enim lobortis scelerisque fermentum. Odio ut enim blandit volutpat maecenas volutpat. Ornare lectus sit amet est placerat in egestas erat. Nisi vitae suscipit tellus mauris a diam maecenas sed. Placerat duis ultricies lacus sed turpis tincidunt id aliquet.</p>
+        <p> My research
+        </p>
     </div>
   `,
 };
@@ -136,24 +136,18 @@ const enJobsPageData = {
   title: "CV",
   items: [
     {
-      title: `Job title`,
-      company: "Company name",
-      startData: "20 April 2019",
-      endDate: "20 April 2022",
-      location: "Berlin",
-      abstract:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
-      achievements: ["achievement 1", "achievement 2", "achievement 3"],
-    },
-    {
-      title: `Job title`,
-      company: "Company name",
-      startData: "20 April 2019",
-      endDate: "",
-      location: "Berlin",
-      abstract:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas purus viverra accumsan in nisl nisi. Arcu cursus vitae congue mauris rhoncus aenean vel elit scelerisque. In egestas erat imperdiet sed euismod nisi porta lorem mollis. Morbi tristique senectus et netus.",
-      achievements: ["achievement 1", "achievement 2"],
-    },
+      // No other content, just an embedded PDF
+    }
+    ]
+  },
+<script>
+  document.addEventListener("DOMContentLoaded", () => {
+    const container = document.getElementById("content_wrapper");
+    if (container) {
+      container.innerHTML = `<iframe src="Yilin-Elaine-Wu/files/CV_Yilin_Wu_25fall.pdf" style="width:100%; height:100vh;" frameborder="0"></iframe>`;
+    }
+  });
+</script>
+
   ],
 };
