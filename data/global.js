d0d800c57ce6c8144f52383bd10dae859e927982
Yilin Elaine Wu <yilinelainewu@gmail.com>
2025-11-24 14:30:39 +0800

    Update globalData with new address and titles

--- data/global.js
+++ data/global.js
@@ -9,9 +9,9 @@ const navbarLinks = {
 const globalData = {
   image: "../files/profile_figure.jpg",
   resume: "../files/CV_Yilin_Wu_25fall.pdf",
-  enAddress: "Chinese Institute for Brain Research(CIBR), Jianzan Bldg., No.26, KEXUEYUAN Rd, Zhongguancun Life Science Park, Changping District, Beijing, China",
+  enAddress: "Chinese Institute for Brain Research(CIBR), Zhongguancun Life Science Park, Changping District, Beijing, China",
   // faAddress: " آدرس شما ",
-  phone: "+1 (510)-229-2508\n+86 152-6275-2105",
+  phone: "+1 (510)-229-2508",
   email: "yilinelainewu@gmail.com",
   // twitterTitle: "Yilin Elaine Wu's Twitter Page",
   // twitter: "https://x.com/elaine190369",
@@ -21,8 +21,8 @@ const globalData = {
   // googleScholar: "@googleScholar",
   // linkedinTitle: "Linkedin Username",
   // linkedin: "https://www.linkedin.com/in/your_username/",
-  githubTitle: "Yilin Elaine Wu's github",
+  githubTitle: "Yilin's github",
   github: "https://github.com/Yilin-Elaine-Wu",
-  orcidTitle: "Yilin Elaine Wu's orcid",
+  orcidTitle: "Yilin's orcid",
   orcid: "https://orcid.org/my-orcid?orcid=0009-0004-0686-398X",
 };
